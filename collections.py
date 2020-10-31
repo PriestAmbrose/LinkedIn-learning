8f90d5dbd36037a015203fceb5d92d7f17bfa0f2
Priest Ambrose <alexey.sitalo@gmail.com>
2020-10-31 04:10:08 -0400

    collections task #1

---
+++ collections.py
@@ -0,0 +1,20 @@
+# Nearest stars to Earth
+stars = [
+'Sol',
+'Alpha Centauri',
+'Barnard',
+'Wolf 359'
+]
+
+print("Fourth nearest star is... ", stars[3])
+# Highest peak on each tectonic plate
+peaks = { 
+'African' :'Kilimanjaro',
+'Antarctic' : 'Vinson',
+'Australian' : 'Puncak Jaya',
+'Eurasian' : 'Everest',
+'North_American' : 'Denali',
+'Pacific' : 'Mauna Kea',
+'South_American' : 'Aconcagua'
+}
+print("hight peak on a Pacific plane is ...", peaks['Pacific'])
