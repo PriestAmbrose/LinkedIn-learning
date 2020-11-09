d8ba2810435a1fb51f048abd5b62bd83a50fbce5
Priest Ambrose <alexey.sitalo@gmail.com>
2020-11-09 11:40:17 -0400

    play around with user snippets

--- Learning-VSC/Mydotnet/wwwroot/js/site.js
+++ Learning-VSC/Mydotnet/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function ThisFunction(id,person) {
+    
+}
+
+try {
+    
+} catch (error) {
+    
+}
+
+console.log('sdf');
+
+// This function rocks
+
+function good(){
+    return good
+}
