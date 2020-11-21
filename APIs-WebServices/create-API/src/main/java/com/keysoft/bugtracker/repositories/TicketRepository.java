32a7a31290ac9d14fe60bf26eaf5b1736799ab6d
Priest Ambrose <alexey.sitalo@gmail.com>
2020-11-21 16:42:28 -0400

    deleted not working project

--- APIs-WebServices/create-API/src/main/java/com/keysoft/bugtracker/repositories/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.keysoft.bugtracker.repositories;
-
-import com.keysoft.bugtracker.domain.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface    TicketRepository extends JpaRepository<Ticket, Integer> {
-
-}
