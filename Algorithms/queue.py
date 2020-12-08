f8525aabdd52a19f82082ad9c1275ccc67b7fe31
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-08 03:36:49 -0400

    implemented queue

---
+++ Algorithms/queue.py
@@ -0,0 +1,19 @@
+# try out the Python queue functions
+
+
+# TODO: create a new empty deque object that will function as a queue
+queue = []
+
+# TODO: add some items to the queue
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
+# TODO: print the queue contents
+print(queue)
+
+# TODO: pop an item off the front of the queue
+queue.pop(0)
+
+print(queue)
