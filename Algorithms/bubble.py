1b4e7c0003375491b2311249395f4aa2c592556d
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-08 22:47:05 -0400

    corrected error in range(,0,-1)

--- Algorithms/bubble.py
+++ Algorithms/bubble.py
@@ -3,7 +3,7 @@
 
 def bubbleSort(dataset):
     # TODO: start with the array length and decrement each time
-    for i in range(len(dataset)-1,1,-1):
+    for i in range(len(dataset)-1,0,-1):
        for j in range(i):
             if dataset[j] > dataset[j+1]:
                 dataset[j], dataset[j+1] = dataset [j+1], dataset[j]
@@ -11,7 +11,7 @@ def bubbleSort(dataset):
 
 
 def main():
-    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53,0]
     bubbleSort(list1)
     print("Result: ", list1)
 
