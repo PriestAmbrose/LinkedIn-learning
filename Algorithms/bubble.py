18c50fcabefb20b7de1732925d421b862a800a7c
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-08 22:35:32 -0400

    bubble sort added

---
+++ Algorithms/bubble.py
@@ -0,0 +1,21 @@
+# Bubble sort algorithm
+
+
+def bubbleSort(dataset):
+    # TODO: start with the array length and decrement each time
+    for i in range(len(dataset)):
+       end = len(dataset)-i-1
+       for j in range(end):
+            if dataset[j] > dataset[j+1]:
+                dataset[j], dataset[j+1] = dataset [j+1], dataset[j]
+            print("Current state: ", dataset)
+
+
+def main():
+    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    bubbleSort(list1)
+    print("Result: ", list1)
+
+
+if __name__ == "__main__":
+    main()
