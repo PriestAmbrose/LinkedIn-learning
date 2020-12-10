e7906fbf03c04b86308316ebbf18a80a83b754ce
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-09 23:25:03 -0400

    overall counting task

---
+++ Algorithms/ValueCounter.py
@@ -0,0 +1,18 @@
+# using a hashtable to count individual items
+
+
+# define a set of items that we want to count
+items = ["apple", "pear", "orange", "banana", "apple",
+         "orange", "apple", "pear", "banana", "orange",
+         "apple", "kiwi", "pear", "apple", "orange"]
+
+# TODO: create a hashtable object to hold the items and counts
+counter = 0
+hashtable = {}
+# TODO: iterate over each item and increment the count for each one
+for item in items:
+    hashtable[item]=""
+    counter+=1
+
+# print the results
+print(counter)
