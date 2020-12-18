d17e5a59feb134af5166eced252920b43aa22a49
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-18 01:19:04 -0400

    global and local parameters

--- Real-world/breakfast.py
+++ Real-world/breakfast.py
@@ -1,9 +1,14 @@
+cheese = 'cheddar'
+
 def make_omelette (ingredient):
     return 'a {} omelette'.format(ingredient)
 
 #print(make_omelette(input("what omelette? ")))
 
 def fancy_omeletee(*ingredient):
+    global cheese
+    cheese = "swiss"
     return "a omelette with {} ingredients".format(len(ingredient))
 
-print(fancy_omeletee('cheese', 'bacon', 'something'))
+#print(fancy_omeletee('cheese', 'bacon', 'something'))
+print(cheese)
