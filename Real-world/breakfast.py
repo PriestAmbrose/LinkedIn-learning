d279efcf9cd69178499fba51f46748ef453fec8f
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-18 01:16:40 -0400

    iterator in parameters

--- Real-world/breakfast.py
+++ Real-world/breakfast.py
@@ -1,4 +1,9 @@
 def make_omelette (ingredient):
     return 'a {} omelette'.format(ingredient)
 
-print(make_omelette(input("what omelette? ")))
+#print(make_omelette(input("what omelette? ")))
+
+def fancy_omeletee(*ingredient):
+    return "a omelette with {} ingredients".format(len(ingredient))
+
+print(fancy_omeletee('cheese', 'bacon', 'something'))
