557b670ca71bc8b28ed1c5a67733cf6f3fe98e96
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-18 01:23:09 -0400

    global varible change

--- Real-world/breakfast.py
+++ Real-world/breakfast.py
@@ -1,14 +1,15 @@
 cheese = 'cheddar'
 
 def make_omelette (ingredient):
-    return 'a {} omelette'.format(ingredient)
+    return 'a {} omelette + {} '.format(ingredient,cheese)
 
-#print(make_omelette(input("what omelette? ")))
+print(make_omelette(input("what omelette? ")))
 
 def fancy_omeletee(*ingredient):
     global cheese
     cheese = "swiss"
     return "a omelette with {} ingredients".format(len(ingredient))
 
-#print(fancy_omeletee('cheese', 'bacon', 'something'))
-print(cheese)
+print(fancy_omeletee('cheese', 'bacon', 'something'))
+
+print(make_omelette(input("what omelette? ")))
