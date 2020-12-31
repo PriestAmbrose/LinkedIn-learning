2a2bfb93468aa52d44568aec0adbde194d6f6be4
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-30 21:54:38 -0400

    some new tests are added (with todos!)

--- TDD/Hangman/src/main/Hangman.java
+++ TDD/Hangman/src/main/Hangman.java
@@ -3,7 +3,19 @@ package main;
 public class Hangman {
 
 	public int countAlphabet(String word, char alphabet) {
-		return 0;
+		int result = 0;
+
+		for (char c: word.toCharArray()){
+			if (c == alphabet) result++;
+		}
+		return result;
 	}
-    
+	
+	public String fetchWord(){
+		return "pizza";
+	}
+
+	public String fetchWord(int requestedLength) {
+		return null;
+	}
 }
--- TDD/Hangman/src/test/TestHangman.java
+++ TDD/Hangman/src/test/TestHangman.java
@@ -6,10 +6,10 @@ import org.junit.Test;
 
 import main.Hangman;
 
-class TestHangman {
+public class TestHangman {
     
     @Test
-    void test_alphabetCountInAWord(){
+    public void test_alphabetCountInAWord(){
         String word = "pizza";
         char alphabet = 'a';
 
@@ -17,5 +17,23 @@ class TestHangman {
         int count = hangman.countAlphabet(word,alphabet);
 
         assertEquals(1,count);
+    }
+
+    @Test
+    
+    public void test_lengthOfFetchedWord(){
+        Hangman hangman = new Hangman();
+        String word = hangman.fetchWord();
+
+        assertTrue(word.length() == 5);
+
+    }
+
+    @Test
+    public void test_lengthOfFetchedWordRandom(){
+        Random random = new Random();
+        int requestedLength = random.nextInt(6) +5;
+        Hangman hangman = new Hangman();
+        String word = hangman.fetchWord(requestedLength);
     }
 }
