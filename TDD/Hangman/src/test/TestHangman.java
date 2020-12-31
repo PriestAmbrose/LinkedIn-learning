b9dcf78694b84801dadf30f6f44f2b097fccb0e3
Priest Ambrose <alexey.sitalo@gmail.com>
2020-12-31 10:14:09 -0400

    made a fixture with @BeforeClass

--- TDD/Hangman/src/test/TestHangman.java
+++ TDD/Hangman/src/test/TestHangman.java
@@ -4,22 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import main.Hangman;
 
 public class TestHangman {
-    
+
+    static Random random;
+    static Hangman hangman;
+    static int requestedLength;
+
+    @BeforeClass
+    public static void setUpClass() {
+        random = new Random();
+        hangman = new Hangman();
+        hangman.loadWords();
+    }
+
+    @Before
+    public void setupTest(){
+        requestedLength = random.nextInt(4)+4;
+    }
+
     @Test
     public void test_alphabetCountInAWord(){
         String word = "pizza";
         char alphabet = 'a';
-
-        Hangman hangman = new Hangman();
-        hangman.loadWords();
         int count = hangman.countAlphabet(word,alphabet);
-
-        assertEquals(1,count);
+        assertEquals(1,count, "wrong count");
     }
 
     
@@ -27,10 +41,9 @@ public class TestHangman {
 
     @Test
     public void test_lengthOfFetchedWordRandom(){
-        Random random = new Random();
-        int requestedLength = random.nextInt(6) +5;
-        Hangman hangman = new Hangman();
-        hangman.loadWords();
+        
+      
+      
         String word = hangman.fetchWord(requestedLength);
 
         assertTrue(requestedLength == word.length());
@@ -39,13 +52,12 @@ public class TestHangman {
     @Test 
 
     public void test_uniquenesssOfFetchWord() {
-        Random random = new Random ();
-        int requestedLength = 0;
+        
+        
         Set<String> usedWordsSet = new HashSet<>();
         int round =0;
         String word = null;
-        Hangman hangman = new Hangman();
-        hangman.loadWords();
+        
 
         while (round<10) {
             requestedLength = random.nextInt(4)+4;
@@ -59,19 +71,17 @@ public class TestHangman {
     @Test
 
     public void test_returnClueBeforeGuess(){
-        Random random = new Random ();
-        int requestedLength = random.nextInt(4)+4;
-        Hangman hangman = new Hangman();
-        hangman.loadWords();
+        
+    
         String word = hangman.fetchWord(requestedLength);
         String clue = "";
         for (int i=0; i<word.length();i++) clue+="-";
-        assertEquals(clue, hangman.getClue(word));
+        assertEquals(clue, hangman.getClue(word), "wrong clue");
     }
 
     @Test
     public void test_returnClueIfGuessedCorrectly(){
-        Hangman hangman = new Hangman();
+        
         String clue = hangman.getClue("pizza");
         String newClue = hangman.getClue("pizza",clue,'z');
         assertEquals("--zz-", newClue);
@@ -79,7 +89,7 @@ public class TestHangman {
 
     @Test
     public void test_returnClueIfGuessedInCorrectly(){
-        Hangman hangman = new Hangman();
+        
         String clue = hangman.getClue("pizza");
         String newClue = hangman.getClue("pizza",clue,'x');
         assertEquals("-----", newClue);
