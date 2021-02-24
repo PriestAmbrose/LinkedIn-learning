b662cd4d5673e0488bfba9e83af021dba2525553
Priest Ambrose <alexey.sitalo@gmail.com>
2021-02-24 18:25:56 -0400

    create objects

---
+++ JS-essential/practice-1/script.js
@@ -0,0 +1,16 @@
+const book = {
+    color: "red",
+    pages: 315,
+    recommended: false,
+    article:{
+        author: "Greg Johnson",
+        title:"Gremlins",
+        year: 1998
+    }
+};
+const phone = {
+    number: "5379646",
+    height: 5,
+    width: 2,
+    "Andr-oid":"yes",
+}
