12ab82814eb981387d09fba59e2d499401fcb468
Priest Ambrose <alexey.sitalo@gmail.com>
2021-02-24 21:45:08 -0400

    challenge 1

---
+++ JS-essential/chalenge-1/book.js
@@ -0,0 +1,11 @@
+const Book = class{
+    constructor(
+        title,
+        pages,
+    ){
+        this.title=title;
+        this.pages=pages;
+    }
+}
+
+export default Book;
---
+++ JS-essential/chalenge-1/script.js
@@ -0,0 +1,9 @@
+import Book from "./book.js"
+
+const b1 = new Book("book1", 301);
+const b2 = new Book("book2", 302);
+const b3 = new Book("book3", 303);
+const b4 = new Book("book4", 304);
+const b5 = new Book("book5", 305);
+
+console.log(b1,b2,b3,b4,b5)
