a0097fb09f004715a17440093ab669701e76af67
Priest Ambrose <alexey.sitalo@gmail.com>
2021-04-03 01:39:18 -0400

    my solution of challenge

---
+++ JS-essential/challenge-3/script.js
@@ -0,0 +1,57 @@
+/**
+ * Challenge: Create an advanced function.
+ * - Loop through backpackObjectArray to create an article with the class "backpack".
+ * - Give the article the ID of the current backpack object.
+ * - Set the inner HTML of the article to the existing HTML output provided in const content.
+ * - Append each backpack object to the <main> element.
+ */
+import backpackObjectArray from "./components/data.js";
+
+
+
+
+const content = (everydayPack)=>`
+    <figure class="backpack__image">
+      <img src=${everydayPack.image} alt="" />
+    </figure>
+    <h1 class="backpack__name">${everydayPack.name}</h1>
+    <ul class="backpack__features">
+      <li class="packprop backpack__volume">Volume:<span> ${
+        everydayPack.volume
+      }l</span></li>
+      <li class="packprop backpack__color">Color:<span> ${
+        everydayPack.color
+      }</span></li>
+      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+      <li class="packprop backpack__pockets">Number of pockets:<span> ${
+        everydayPack.pocketNum
+      }</span></li>
+      <li class="packprop backpack__strap">Left strap length:<span> ${
+        everydayPack.strapLength.left
+      } inches</span></li>
+      <li class="packprop backpack__strap">Right strap length:<span> ${
+        everydayPack.strapLength.right
+      } inches</span></li>
+      <li class="feature backpack__lid">Lid status:<span> ${
+        everydayPack.lidOpen ? "open" : "closed"
+      }</span></li>
+    </ul>
+  
+`;
+
+const main = document.querySelector(".maincontent");
+
+
+
+const newArray = backpackObjectArray.map((item)=>{
+  const newArticle = document.createElement("article");
+  newArticle.classList.add("backpack");
+  newArticle.setAttribute("id", item.id);
+  newArticle.innerHTML = content(item);
+  main.append(newArticle);
+  return item;
+})
+
+
+
+
