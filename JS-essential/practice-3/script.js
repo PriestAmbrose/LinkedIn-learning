cfaa3bc919794f77c4b997b4caace3575986e7b0
Priest Ambrose <alexey.sitalo@gmail.com>
2021-04-02 21:39:19 -0400

    creating and changing the body of the document

--- JS-essential/practice-3/script.js
+++ JS-essential/practice-3/script.js
@@ -1,4 +1,9 @@
 function addHeader(header){
+    if (!document.body){
+        const body = document.createElement("body");
+        document.body = body;
+        console.log(document.body);
+    }
     document.body.innerHTML+=`<h1>${header}</h1>`
 }
 const addStyle = function(tag, color){
@@ -10,3 +15,8 @@ document.body.innerHTML+= `
 `
 };
 var doSmth = () => document.body.innerHTML+="<script src=\"script.js\"> hi</script>"
+
+
+addHeader("Helllo");
+addStyle("h1", "pink");
+doSmth();
