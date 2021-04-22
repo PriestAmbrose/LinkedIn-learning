72fdaa867d91efb51ade152c2aaf36e9dcff4fbd
Priest Ambrose <alexey.sitalo@gmail.com>
2021-04-22 02:45:45 -0400

    some more tests and fix .babelrc error

---
+++ TDD-JS/UnitTesting/src/letterCount.test.js
@@ -0,0 +1,18 @@
+import {expect} from 'chai'
+import {getLetterCount} from './letterCount';
+
+
+describe('getLetterCount - basic functionality', ()=>{
+    it('returns an empty object when passed an empty string', ()=>{
+        const expected = {};
+        const actual = getLetterCount('');
+        expect(actual).to.deep.equal(expected);
+
+    });
+    
+    it('return the correct letter coutn for a word with only one letter', ()=>{
+        const expected = {c:1, a:1, t:1};
+        const actual = getLetterCount('cat');
+        expect(actual).to.deep.equal(expected);
+    })
+})
