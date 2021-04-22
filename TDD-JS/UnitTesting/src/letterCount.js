00e94031b8965d2f9a66957044381435e03cedbc
Priest Ambrose <alexey.sitalo@gmail.com>
2021-04-22 03:02:35 -0400

    added functionality to count all letters

--- TDD-JS/UnitTesting/src/letterCount.js
+++ TDD-JS/UnitTesting/src/letterCount.js
@@ -1,1 +1,13 @@
-export const getLetterCount = string => ({});
+export const getLetterCount = string => {
+    const letters = string.split('');
+    const letterCount = {};
+    letters.forEach(element => {
+        if (!letterCount[element]){
+            letterCount[element]=1;
+        }
+        else {
+            letterCount[element]++;
+        }
+    });
+    return letterCount;
+};
--- TDD-JS/UnitTesting/src/letterCount.test.js
+++ TDD-JS/UnitTesting/src/letterCount.test.js
@@ -14,5 +14,11 @@ describe('getLetterCount - basic functionality', ()=>{
         const expected = {c:1, a:1, t:1};
         const actual = getLetterCount('cat');
         expect(actual).to.deep.equal(expected);
-    })
+    });
+
+    it('returns the correct letter count with word with more then one certain letters', ()=>{
+        const expected = {m:1,i:4, s:4, p:2};
+        const actual = getLetterCount('mississippi');
+        expect(actual).to.deep.equal(expected);
+    });
 })
