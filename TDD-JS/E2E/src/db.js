d258bf7061a4bdb9e8eb3d8c8c9dd24675787b85
Priest Ambrose <alexey.sitalo@gmail.com>
2021-04-24 03:03:50 -0400

    using env for production/test

--- TDD-JS/E2E/src/db.js
+++ TDD-JS/E2E/src/db.js
@@ -1,9 +1,13 @@
 import {MongoClient} from 'mongodb'
 
+const DB_NAME = process.env.NODE_ENV === 'test'
+? 'TEST_DB'
+: 'PROD_DB';
+
 
 export const getUserByUsername = async username => {
     const client = await MongoClient.connect(
-        'mongodb://localhost:27017/TEST_DB',
+        'mongodb://localhost:27017/${DB_NAME}',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true
