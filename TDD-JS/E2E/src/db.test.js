ee1196e82ed2fc681fa644fa381427674bfd6fdb
Priest Ambrose <alexey.sitalo@gmail.com>
2021-04-24 03:25:19 -0400

    add test-helpers

--- TDD-JS/E2E/src/db.test.js
+++ TDD-JS/E2E/src/db.test.js
@@ -1,18 +1,10 @@
-import {MongoClient} from 'mongodb'
 import {getUserByUsername} from './db'
 import {expect} from 'chai'
+import { getDatabaseData, resetDatabase, setDatabaseData } from './test-helper';
 
 describe('getUserByUsername',  ()=>{
     it('get the correct user from othe database given a usernam',async ()=>{
-        const client = await MongoClient.connect(
-            'mongodb://localhost:27017/TEST_DB',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            },
-        );
-        const db = client.db('TEST_DB');
-        //tets
+        
         const fakeData = [{
             id: '123',
             username :'abc',
@@ -22,12 +14,13 @@ describe('getUserByUsername',  ()=>{
             username: 'wrong',
             email: 'wrong@wrong.com',
         }];
-        await db.collection('users').insertMany(fakeData);
-        const actual  = await getUserByUsername('abc');
-        const finalDBState  = await db.collection('users').find().toArray();//real state of db
-        await db.dropDatabase(); //reset databse doesnt' effect other tests
-        client.close();//close before assertions in case they fail
 
+        await setDatabaseData('users',fakeData);
+      
+        const actual  = await getUserByUsername('abc');
+        const finalDBState  = await getDatabaseData('users');
+        
+        await resetDatabase();
         const expected = {
             id: '123',
             username :'abc',
