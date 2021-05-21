f8bddd41eccb3b4918e06cc6e921464715d7ca4c
Priest Ambrose <alexey.sitalo@gmail.com>
2021-05-21 14:59:19 -0400

    search template string

--- ECMA6/script.js
+++ ECMA6/script.js
@@ -3,3 +3,4 @@ console.log(planet.endsWith("th"))
 console.log(planet.startsWith("Ear"))
 console.log(planet.includes("h"))
 console.log(planet.search("art"))
+console.log(`${planet}`.search("th"));    
