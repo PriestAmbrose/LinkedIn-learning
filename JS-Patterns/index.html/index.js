614478e45be60ee065e80d1cb0fb9e0e28629f05
Priest Ambrose <alexey.sitalo@gmail.com>
2021-05-27 01:52:41 -0400

    constructor pattern

--- JS-Patterns/index.html/index.js
+++ JS-Patterns/index.html/index.js
@@ -1,9 +1,22 @@
-const calc = ()=>{
-    return 4*4;
+class Car {
+    constructor(doors,engine,color){
+        this.doors=doors;
+        this.engine =engine;
+        this.color =color;
+        
+    }
 }
 
-const printCalc = (callback)=>{
-    console.log(callback());
+class SUV extends Car{
+    constructor(doors,engine, color){
+        super(doors,engine, color);
+        this.wheels = 4
+    }
 }
 
-printCalc(calc);
+
+
+const civic = new Car(4, 'V6', 'grey');
+const cx5 = new SUV(4, 'V8', 'red');
+console.log(civic);
+console.log(cx5);
