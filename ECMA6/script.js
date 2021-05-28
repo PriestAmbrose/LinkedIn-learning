df343aba77f631f344a618d64e0d8ca18274ff40
Priest Ambrose <alexey.sitalo@gmail.com>
2021-05-27 23:35:02 -0400

    promise is return from a function

--- ECMA6/script.js
+++ ECMA6/script.js
@@ -4,3 +4,18 @@ console.log(planet.startsWith("Ear"))
 console.log(planet.includes("h"))
 console.log(planet.search("art"))
 console.log(`${planet}`.search("th"));    
+
+const delay = (seconds)=>
+    new Promise((resolve, reject) =>{
+        if (typeof seconds !=="number"){
+            reject(
+                new Error("seconds must be a number")
+            );
+        }
+    setTimeout(resolve, seconds * 1000);
+    });
+
+console.log("Zero seconds");
+delay(1).then(()=>console.log("1 sec"));
+
+    
--- JS-Patterns/index.html/index.js
+++ JS-Patterns/index.html/index.js
@@ -1,27 +1,21 @@
-let instance = null;
-
-
-
 class Car {
     constructor(doors,engine,color){
-        if (!instance){
-            this.doors=doors;
-            this.engine =engine;
-            this.color =color;
-            instance = this;
-        }
-        else {
-            return instance;
-        }
-        
+        this.doors=doors;
+        this.engine =engine;
+        this.color =color;
         
     }
 }
 
-
-
+class carFactory {
+    createCar(type){
+        switch(type){
+            
+        }
+    }
+}
 
 const civic = new Car(4, 'V6', 'grey');
-const cx5 = new Car(2, 'V8', 'red');
+const cx5 = new SUV(4, 'V8', 'red');
 console.log(civic);
 console.log(cx5);
