0a276f92e200feb7c91268ccb9a76ce29471260a
Priest Ambrose <alexey.sitalo@gmail.com>
2021-05-29 16:58:08 -0400

    demonstration of what is class

---
+++ JS-Classes/app.js
@@ -0,0 +1,15 @@
+class Car {
+    constructor(doors, engine, color){
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+     
+    carStats(){
+        return `This car has ${this.doors} doors, a ${this.engine} engine and a ${this.color}`
+    }
+}
+
+const cx5 = new Car(4, 'V6', 'grey');
+console.log(cx5);
+console.log(cx5.carStats());
