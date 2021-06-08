9719486c3b61d1407b6fb934bb94aa7ac43288b1
Priest Ambrose <alexey.sitalo@gmail.com>
2021-06-07 21:52:27 -0400

    explore california fictitious website

---
+++ Async-JS/explore-California/_scripts/ajax.js
@@ -0,0 +1,37 @@
+// add global variable containing XHR object here
+
+
+// add get() function here
+
+
+function tempToF(kelvin) {
+    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+}
+
+function successHandler(data) {
+    const dataObj = JSON.parse(data);
+    const weatherDiv = document.querySelector('#weather');
+    const weatherFragment = `
+        <h1>Weather</h1>
+        <h2 class="top">
+        <img
+            src="http://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
+            alt="${dataObj.weather[0].description}"
+            width="50"
+            height="50"
+        />${dataObj.name}
+        </h2>
+        <p>
+        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
+        </p>
+    `
+    weatherDiv.innerHTML = weatherFragment;
+    weatherDiv.classList.remove('hidden');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
+    // add get() function call here
+    
+});
