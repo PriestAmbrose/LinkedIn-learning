a5d49179c800d5e70f57794efe9296e6e76b29c5
Priest Ambrose <alexey.sitalo@gmail.com>
2021-07-03 17:54:31 -0400

    non-blocking image procesing

--- Async-JS/webworkers/_scripts/filter-worker.js
+++ Async-JS/webworkers/_scripts/filter-worker.js
@@ -59,3 +59,11 @@ const applyFilter = (filter, level, image) => {
   console.log("Filtering: %d ms", totalTime);
   return results;
 };
+
+this.addEventListener('message', function(e){
+  // this.console.log('Message received from main script:');
+  // this.console.log(e.data);
+  const workerResult = applyFilter(e.data.filter, e.data.level, e.data.image);
+  //this.console.log(workerResult);
+  this.postMessage(workerResult)
+})
--- Async-JS/webworkers/_scripts/filters.js
+++ Async-JS/webworkers/_scripts/filters.js
@@ -15,6 +15,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const outputCanvas = document.querySelector('#output');
   const ctx = outputCanvas.getContext('2d');
   let selectedFilter = '';
+  const worker= new Worker('_scripts/filter-worker.js');
+  //worker.postMessage('Hello worker!');
+  //console.log('Message sent to worker');
 
   const getImageData = (image) => {
     const tempCanvas = document.createElement('canvas');
@@ -26,62 +29,65 @@ document.addEventListener('DOMContentLoaded', function() {
     return imageDataObj;
   };
 
-  const delay = (ms) => {
-    const startPoint = new Date().getTime()
-    while (new Date().getTime() - startPoint <= ms) {/* wait */}
-  };
+  // const delay = (ms) => {
+  //   const startPoint = new Date().getTime()
+  //   while (new Date().getTime() - startPoint <= ms) {/* wait */}
+  // };
 
-  const grayscaleImage = (pixels) => {
-    var d = pixels.data;
-    for (var i = 0; i < d.length; i += 4) {
-      var r = d[i];
-      var g = d[i + 1];
-      var b = d[i + 2];
-      // CIE luminance for the RGB
-      // The human eye is bad at seeing red and blue, so we de-emphasize them.
-      var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-      d[i] = d[i + 1] = d[i + 2] = v
-    }
-    return pixels;
-  };
+  // const grayscaleImage = (pixels) => {
+  //   var d = pixels.data;
+  //   for (var i = 0; i < d.length; i += 4) {
+  //     var r = d[i];
+  //     var g = d[i + 1];
+  //     var b = d[i + 2];
+  //     // CIE luminance for the RGB
+  //     // The human eye is bad at seeing red and blue, so we de-emphasize them.
+  //     var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  //     d[i] = d[i + 1] = d[i + 2] = v
+  //   }
+  //   delay(1000);
+  //   return pixels;
+  // };
 
-  const brightnessImage = (adjustment, pixels) => {
-    var d = pixels.data;
-    for (var i = 0; i < d.length; i += 4) {
-      d[i] += adjustment;
-      d[i+1] += adjustment;
-      d[i+2] += adjustment;
-    }
-    return pixels;
-  };
+  // const brightnessImage = (adjustment, pixels) => {
+  //   var d = pixels.data;
+  //   for (var i = 0; i < d.length; i += 4) {
+  //     d[i] += adjustment;
+  //     d[i+1] += adjustment;
+  //     d[i+2] += adjustment;
+  //   }
+  //   delay(1000);
+  //   return pixels;
+  // };
 
-  const thresholdImage = (level, pixelData) => {
-    const thresholdLevel = level;    // 0-255
-    var d = pixelData.data;
-    for (var i = 0; i < d.length; i += 4) {
-      var r = d[i];
-      var g = d[i + 1];
-      var b = d[i + 2];
-      var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= thresholdLevel) ? 255 : 0;
-      d[i] = d[i + 1] = d[i + 2] = v
-    }
-    return pixelData;
-  };
+  // const thresholdImage = (level, pixelData) => {
+  //   const thresholdLevel = level;    // 0-255
+  //   var d = pixelData.data;
+  //   for (var i = 0; i < d.length; i += 4) {
+  //     var r = d[i];
+  //     var g = d[i + 1];
+  //     var b = d[i + 2];
+  //     var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= thresholdLevel) ? 255 : 0;
+  //     d[i] = d[i + 1] = d[i + 2] = v
+  //   }
+  //   delay(1000);
+  //   return pixelData;
+  // };
 
-  const applyFilter = (filter, level, image) => {
-    const startTime = new Date().getTime();
-    let results;
-    if (filter === 'brightness') {
-      results = brightnessImage(level, image);
-    } else if (filter === 'threshold') {
-      results = thresholdImage(level, image);
-    } else if (filter === 'grayscale') {
-      results = grayscaleImage(image);
-    }
-    const totalTime = new Date().getTime() - startTime;
-    console.log("Filtering: %d ms", totalTime);
-    return results;
-  };
+  // const applyFilter = (filter, level, image) => {
+  //   const startTime = new Date().getTime();
+  //   let results;
+  //   if (filter === 'brightness') {
+  //     results = brightnessImage(level, image);
+  //   } else if (filter === 'threshold') {
+  //     results = thresholdImage(level, image);
+  //   } else if (filter === 'grayscale') {
+  //     results = grayscaleImage(image);
+  //   }
+  //   const totalTime = new Date().getTime() - startTime;
+  //   console.log("Filtering: %d ms", totalTime);
+  //   return results;
+  // };
 
   const resetCanvas = () => {
     const imageDataObj = getImageData(image);
@@ -96,8 +102,13 @@ document.addEventListener('DOMContentLoaded', function() {
     selectedFilter = 'grayscale';
     const imageDataObj = getImageData(image);
     const level = Number(range.value);
-    const results = applyFilter('grayscale', level, imageDataObj);
-    displayFilteredImage(ctx, results);
+    //const results = applyFilter('grayscale', level, imageDataObj);
+    worker.postMessage({
+      filter: selectedFilter,
+      level: level,
+      image:imageDataObj,
+    });
+    //displayFilteredImage(ctx, results);
     slider.classList.add('hidden');    
   });
 
@@ -107,8 +118,13 @@ document.addEventListener('DOMContentLoaded', function() {
     range.max = 100;
     range.value = 50;
     const level = Number(range.value);
-    const results = applyFilter('brightness', level, imageDataObj);
-    displayFilteredImage(ctx, results);
+    //const results = applyFilter('brightness', level, imageDataObj);
+    worker.postMessage({
+      filter: selectedFilter,
+      level: level,
+      image:imageDataObj,
+    });
+    //displayFilteredImage(ctx, results);
     slider.classList.remove('hidden');
   });
 
@@ -118,8 +134,13 @@ document.addEventListener('DOMContentLoaded', function() {
     range.max = 255;
     range.value = 127;
     const level = Number(range.value);
-    const results = applyFilter('threshold', level, imageDataObj);
-    displayFilteredImage(ctx, results);
+    //const results = applyFilter('threshold', level, imageDataObj);
+    worker.postMessage({
+      filter: selectedFilter,
+      level: level,
+      image:imageDataObj,
+    });
+    //displayFilteredImage(ctx, results);
     slider.classList.remove('hidden');
   });
 
@@ -143,4 +164,10 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   
   image.addEventListener('load', resetCanvas);
+
+  worker.addEventListener('message', function(e){
+    console.log('Received data back from worker:');
+    console.log(e.data);
+    displayFilteredImage(ctx, e.data);
+  })
 });
